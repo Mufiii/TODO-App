4ca4814e8a52d23916edc6b392c989ef4a8ac473
mufiii <mufeesrahman9795@gmail.com>
2023-04-05 09:16:22 +0530

    'update'

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import TodoWrapper from './Component/TodoWrapper'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <TodoWrapper/>
+    </div>
+  )
+}
+
+export default App
